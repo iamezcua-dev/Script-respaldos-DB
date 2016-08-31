4c18d8a949f58ad08f8f6ec7133285276b1caef0
Isaac Eduardo Amezcua Bustos <iamezcua@dattlas.com>
2016-08-31 17:34:48 -0500

    Automatically send backups to Arturo's server
    - Change backup retain policy to 3, plus the new one (4 in total).
    - fix database backup command issue by specifying pg_dump command full path.
    - Redirect "pg_dump" command error stream to "backupProcess.err" file.
    - "cd" and "gzip" commands are in separated lines, for better code reading.

--- backup.sh
+++ backup.sh
@@ -1,44 +1,23 @@
 #!/bin/bash
-
 # This script runs a Postgres backup of the Palantir database.
 # It is somewhat hard-coded for PRODUCTION file paths,
 # but it should be fairly easy to convert for STAGING.
-
 # Currently this script is deployed on prod-db01 to /opt/palantir/backup/daily
-
 #####################
 ### Configuration ###
 #####################
-
 # The number of files to retain going backward
-RETAIN_BACKUPS=15
-
+RETAIN_BACKUPS=3
 # Database name
 DATABASE="prod_pgr"
-
 # Target folder for writing files
 FOLDER=/opt/palantir/backup/daily/files
-
 ##########################
 ### DONE CONFIGURATION ###
 ##########################
 
-NOW=$(date +"%Y-%m-%d")
-FILENAME="production-backup-$NOW.sql"
-TARGET="$FOLDER/$FILENAME"
-echo "[$(date)] Backing up PRODUCTION database to: $TARGET"
-
-pg_dump --clean $DATABASE > $TARGET
-echo "[$(date)] Backup complete. Compressing..."
-cd $FOLDER; gzip $FILENAME
-
-##
-##TODO: add in code for copying backup to SFTP server
-##
-
-
 # Remove older files (so we don't eat up all of the disk)
-n=1
+n=0
 cd $FOLDER
 for f in `ls -t *.gz`; do
   n=$(( n + 1 ))
@@ -49,4 +28,25 @@ for f in `ls -t *.gz`; do
   fi
 done
 
+# Create database backup
+NOW=$(date +"%Y-%m-%d")
+FILENAME="production-backup-$NOW.sql"
+TARGET="$FOLDER/$FILENAME"
+echo "[$(date)] Backing up PRODUCTION database to: $TARGET"
+/opt/palantir/postgresql/bin/pg_dump --clean $DATABASE > $TARGET 2> backupProcess.err
+echo "[$(date)] Backup complete. Compressing..."
+gzip $TARGET
+
+# Copying generated backup to Arturo's server via SFTP.
+SFTPUSER=PalantirBackup
+SERVER=10.3.248.19
+PORT=2222
+echo "[$(date)] Sending a copy of generated backup file towards Arturo's Server..."
+sftp -oPort=$PORT $SFTPUSER@$SERVER << sFtpTask
+        cd RESPALDOS
+        put $TARGET.gz
+        quit
+sFtpTask
+
+echo "[INF] Backup file copy process finished."
 echo "[$(date)] Done."
